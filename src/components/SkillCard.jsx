3cd30da8182bb5617537df83bf98c35c331997a9
isaaacme <holla@isaaac.me>
2020-11-15 13:17:13 +0200

    homepage still

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Styles from "../styles/Skill.module.css";
-function SkillCard({ title, description }) {
+function SkillCard({ title, description, cta }) {
   return (
     <div className={Styles.Skill}>
       <h3 className={Styles.title}>{title}</h3>
       <p className={Styles.description}>{description}</p>
-      <div className={Styles.cta}>let's work together</div>
+      <div className={Styles.cta}>{cta}</div>
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,18 +10,22 @@ function Skills(props) {
         <SkillCard
           title="Web development"
           description="The ability to look at products and user flows from a business prespetive allows me to create data driven decisions that guide my design to satisfy both visual communication needs and business goals"
+          cta="show me"
         />
         <SkillCard
           title="ux/ui design"
           description="From wordpress and webflow site to custom hand coded React Applications, along my journey I\'ve collected all the tools to go from Idea to Execution"
+          cta="show me"
         />
         <SkillCard
           title="webmachines podcast"
           description="A podcast about the more interesting and least talked about subjects in the no-code space. Free form interviews with makers, Founders & visual developers"
+          cta="listen to it"
         />
         <SkillCard
           title="samimkashim .xyz"
           description="Exclusive clothing brand from the weird side of the internet, brought to you risk free of dark web hackers and internet lurkers"
+          cta="buy cool sh/t"
         />
       </div>
     </section>
