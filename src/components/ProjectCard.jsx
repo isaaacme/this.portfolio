3b63b6648a40cd05f3b7d9df27c20a10f9f80778
isaaacme <holla@isaaac.me>
2020-11-26 11:42:27 +0200

    projects cards ftw

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+import Styles from "../styles/ProjectCard.module.css";
+function ProjectCard({ title, description, cta, url, logo, alt }) {
+  return (
+    <div className={Styles.ProjectCard}>
+      <img src={logo} alt={alt} />
+      <div className={Styles.ProjectInfo}>
+        <h3 className={Styles.title}>{title}</h3>
+        <p className={Styles.description}>{description}</p>
+        <Link href={`${url}`}>
+          <a>
+            <div className={Styles.cta}>{cta}</div>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
