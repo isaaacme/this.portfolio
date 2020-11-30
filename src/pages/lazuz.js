55fad1a7fecf8a6c2ef5379c6efdf75a47a87606
isaaacme <holla@isaaac.me>
2020-11-30 12:17:02 +0200

    moving content to dedicated data folder

--- src/pages/lazuz.js
+++ src/pages/lazuz.js
@@ -1,7 +1,10 @@
-import Head from "next/head";
-import StyleSheet from "../styles/Layout.module.css";
-import InnerPageHeader from "../components/InnerPageHeader";
-import ProjectPage from "../components/ProjectPage";
+import Head from 'next/head';
+import LinkBlock from '../components/LinkBlock';
+import Header from '../components/Header';
+import HighlightBox from '../components/HighlightBox';
+import ProjectData from '../data/projects.json';
+import StyleSheet from '../styles/Layout.module.css';
+import Styles from '../styles/ProjectPage.module.css';
 export default function Lazuz() {
   return (
     <div className={StyleSheet.PageContainer}>
@@ -12,8 +15,52 @@ export default function Lazuz() {
           content="width=device-width, initial-scale=1"
         ></meta>
       </Head>
-      <InnerPageHeader />
-      <ProjectPage />
+      <Header />
+      <div className={Styles.ProjectTitle}>
+        <h1>{ProjectData.projects.lazuz.projectTitle}</h1>
+        <p>{ProjectData.projects.lazuz.projectBlurb}</p>
+      </div>
+
+      <main className={Styles.ProjectPageContent}>
+        <article className={Styles.ContentBlock}>
+          <h3>The Challenge</h3>
+          <p>{ProjectData.projects.lazuz.theChallenge}</p>
+          <h3>The Solution</h3>
+          <p>{ProjectData.projects.lazuz.theSolution}</p>
+        </article>
+        <HighlightBox
+          symbol="🤸‍♀️"
+          label="person doing roll"
+          text={`${ProjectData.projects.lazuz.highLight}`}
+        />
+        <article className={Styles.ContentBlock}>
+          <h3>{ProjectData.projects.lazuz.InfoTitle}</h3>
+          <p>{ProjectData.projects.lazuz.InfoText}</p>
+          <h3>{ProjectData.projects.lazuz.InfoTitle02}</h3>
+          <p>{ProjectData.projects.lazuz.InfoText02}</p>
+        </article>
+        <article className={Styles.ContentBlock}>
+          <h3>{ProjectData.projects.lazuz.InfoTitle03}</h3>
+          <p>{ProjectData.projects.lazuz.InfoText03}</p>
+          <img src="/imgs/survey-img.png" alt="" />
+        </article>
+        <article className={`${Styles.ContentBlock} ${Styles.ContentBlockImg}`}>
+          <h3>{ProjectData.projects.lazuz.InfoTitle04}</h3>
+          <p>{ProjectData.projects.lazuz.InfoText04}</p>
+          <img src="/imgs/axure.png" alt="" />
+          <LinkBlock
+            url="https://z4effq.axshare.com/onboarding.html"
+            cta="Check the Axure"
+          />
+        </article>
+        <article>
+          <h3>UI Design</h3>
+          <div className={Styles.UiScreens}>
+            <img src="/imgs/lazuz-ui-01.png" alt="" />
+            <img src="/imgs/lazuz-ui-02.png" alt="" />
+          </div>
+        </article>
+      </main>
     </div>
   );
 }
