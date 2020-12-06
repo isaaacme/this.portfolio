a02be850885eba4862bc8bf8462bb36ba9ccee68
isaaacme <holla@isaaac.me>
2020-12-06 06:43:06 +0200

    propagating data through the pages

--- src/pages/logofolio.js
+++ src/pages/logofolio.js
@@ -1,5 +1,10 @@
-import Head from "next/head";
-import StyleSheet from "../styles/Layout.module.css";
+import Head from 'next/head';
+import LinkBlock from '../components/LinkBlock';
+import Header from '../components/Header';
+import HighlightBox from '../components/HighlightBox';
+import ProjectData from '../data/projects.json';
+import StyleSheet from '../styles/Layout.module.css';
+import Styles from '../styles/ProjectPage.module.css';
 export default function Logofolio() {
   return (
     <div className={StyleSheet.PageContainer}>
@@ -10,7 +15,52 @@ export default function Logofolio() {
           content="width=device-width, initial-scale=1"
         ></meta>
       </Head>
-      <p>sdvmds</p>
+      <Header />
+      <div className={Styles.ProjectTitle}>
+        <h1>{ProjectData.projects.logofolio.projectTitle}</h1>
+        <p>{ProjectData.projects.logofolio.projectBlurb}</p>
+      </div>
+
+      <main className={Styles.ProjectPageContent}>
+        <article className={Styles.ContentBlock}>
+          <h3>The Challenge</h3>
+          <p>{ProjectData.projects.logofolio.theChallenge}</p>
+          <h3>The Solution</h3>
+          <p>{ProjectData.projects.logofolio.theSolution}</p>
+        </article>
+        <HighlightBox
+          symbol="🤸‍♀️"
+          label="person doing roll"
+          text={`${ProjectData.projects.logofolio.highLight}`}
+        />
+        <article className={Styles.ContentBlock}>
+          <h3>{ProjectData.projects.logofolio.InfoTitle}</h3>
+          <p>{ProjectData.projects.logofolio.InfoText}</p>
+          <h3>{ProjectData.projects.logofolio.InfoTitle02}</h3>
+          <p>{ProjectData.projects.logofolio.InfoText02}</p>
+        </article>
+        <article className={Styles.ContentBlock}>
+          <h3>{ProjectData.projects.logofolio.InfoTitle03}</h3>
+          <p>{ProjectData.projects.logofolio.InfoText03}</p>
+          <img src="/imgs/survey-img.png" alt="" />
+        </article>
+        <article className={`${Styles.ContentBlock} ${Styles.ContentBlockImg}`}>
+          <h3>{ProjectData.projects.logofolio.InfoTitle04}</h3>
+          <p>{ProjectData.projects.logofolio.InfoText04}</p>
+          <LinkBlock
+            url="https://z4effq.axshare.com/onboarding.html"
+            cta="Check the Axure"
+          />
+          <img src="/imgs/axure.png" alt="" />
+        </article>
+        <article>
+          <h3>UI Design</h3>
+          <div className={Styles.UiScreens}>
+            <img src="/imgs/logofolio-ui-01.png" alt="" />
+            <img src="/imgs/logofolio-ui-02.png" alt="" />
+          </div>
+        </article>
+      </main>
     </div>
   );
 }
--- src/pages/schooolday.js
+++ src/pages/schooolday.js
@@ -1,8 +1,13 @@
-import Head from "next/head";
-import Styles from "../styles/Layout.module.css";
+import Head from 'next/head';
+import LinkBlock from '../components/LinkBlock';
+import Header from '../components/Header';
+import HighlightBox from '../components/HighlightBox';
+import ProjectData from '../data/projects.json';
+import StyleSheet from '../styles/Layout.module.css';
+import Styles from '../styles/ProjectPage.module.css';
 export default function Schooolday() {
   return (
-    <div className={Styles.PageContainer}>
+    <div className={StyleSheet.PageContainer}>
       <Head>
         <title>isaac feldman - web stuff maker</title>
         <meta
@@ -10,6 +15,52 @@ export default function Schooolday() {
           content="width=device-width, initial-scale=1"
         ></meta>
       </Head>
+      <Header />
+      <div className={Styles.ProjectTitle}>
+        <h1>{ProjectData.projects.schooolday.projectTitle}</h1>
+        <p>{ProjectData.projects.schooolday.projectBlurb}</p>
+      </div>
+
+      <main className={Styles.ProjectPageContent}>
+        <article className={Styles.ContentBlock}>
+          <h3>The Challenge</h3>
+          <p>{ProjectData.projects.schooolday.theChallenge}</p>
+          <h3>The Solution</h3>
+          <p>{ProjectData.projects.schooolday.theSolution}</p>
+        </article>
+        <HighlightBox
+          symbol="🤸‍♀️"
+          label="person doing roll"
+          text={`${ProjectData.projects.schooolday.highLight}`}
+        />
+        <article className={Styles.ContentBlock}>
+          <h3>{ProjectData.projects.schooolday.InfoTitle}</h3>
+          <p>{ProjectData.projects.schooolday.InfoText}</p>
+          <h3>{ProjectData.projects.schooolday.InfoTitle02}</h3>
+          <p>{ProjectData.projects.schooolday.InfoText02}</p>
+        </article>
+        <article className={Styles.ContentBlock}>
+          <h3>{ProjectData.projects.schooolday.InfoTitle03}</h3>
+          <p>{ProjectData.projects.schooolday.InfoText03}</p>
+          <img src="/imgs/survey-img.png" alt="" />
+        </article>
+        <article className={`${Styles.ContentBlock} ${Styles.ContentBlockImg}`}>
+          <h3>{ProjectData.projects.schooolday.InfoTitle04}</h3>
+          <p>{ProjectData.projects.schooolday.InfoText04}</p>
+          <LinkBlock
+            url="https://z4effq.axshare.com/onboarding.html"
+            cta="Check the Axure"
+          />
+          <img src="/imgs/axure.png" alt="" />
+        </article>
+        <article>
+          <h3>UI Design</h3>
+          <div className={Styles.UiScreens}>
+            <img src="/imgs/schooolday-ui-01.png" alt="" />
+            <img src="/imgs/schooolday-ui-02.png" alt="" />
+          </div>
+        </article>
+      </main>
     </div>
   );
 }
