85e1fccf92847905e2e238ecbd6d5ea3d6348c3a
isaaacme <holla@isaaac.me>
2020-12-22 09:06:17 +0200

    seo bullshit continues

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,12 @@ export default function Home() {
           <meta property="og:url" content="https://isaaac.me/"></meta>
           <meta property="og:title" content="Hi, I'm Isaac. a Web Stuff Maker"></meta>
           <meta property="og:description" content="or a Web developer & Designer resolving end to end business problems, if you're not into abbreviations."></meta>
-          <meta property="og:image" content="https://isaaac.me/card.webp"></meta>
+          <meta property="og:image" content="https://isaaac.me/card.png"></meta>
           <meta property="twitter:card" content="summary_large_image"></meta>
           <meta property="twitter:url" content="https://isaaac.me/"></meta>
           <meta property="twitter:title" content="Hi, I'm Isaac. a Web Stuff Maker"></meta>
           <meta property="twitter:description" content="or a Web developer & Designer resolving end to end business problems, if you're not into abbreviations."></meta>
-          <meta property="twitter:image" content="https://isaaac.me/card.webp"></meta>
+          <meta property="twitter:image" content="https://isaaac.me/card.png"></meta>
           <title>Hi, I'm Isaac. a Web Stuff Maker</title>
       </Head>
       <Header />
